52abe4bddc3d8fb446d2c16290fcca79dda1ccec
Nils Henrik Aaman <softwareuabhnk@gmail.com>
2018-03-08 16:10:26 +0100

    vpp project Chapter 14 'Built in Functional Interfaces' - First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.function.Predicate;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		BookCollection bc = new BookCollection();
+		
+        //Predicate<Book> myLambda = book -> book.getTitle().toLowerCase().contains("men");
+		
+		// Passing the the lambda in to the function
+		List<Book> foundBooks = bc.findBooks(book -> book.getTitle().toLowerCase().contains("men"));
+	
+		for (Book nextBook : foundBooks) {
+			System.out.println (nextBook.getTitle() + " by " + nextBook.getAuthor());
+		}
+	}
+}
